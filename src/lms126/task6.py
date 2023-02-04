778491ca8c2006377e19766333e739492a18c67d
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-05 00:06:03 +0100

    Update task6.py
    multiples of 5

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task6():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT x
+    Loop x from 1 to 100
+    IF x multiplies of 5 then
+    OUTPUT x
+    END if
+    END loop
+    """
