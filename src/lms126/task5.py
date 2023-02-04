209b0cba12635fa79a10320e9c7b2d370aaf802e
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-04 23:57:08 +0100

    Update task5.py
    corresponding day

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,24 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT x
+    x = from 1 to 8
+    IF x = 1 
+    PRINT Monday
+    IF x = 2 
+    PRINT Tuesday
+    IF x = 3
+    PRINT Wendsday
+    IF x = 4
+    PRINT Thursday
+    IF x = 5
+    PRINT Friday
+    IF x = 6
+    PRINT Saturday
+    IF x = 7 
+    PRINT Sunday
+    IF x = 8
+    Print error
+
+    """
