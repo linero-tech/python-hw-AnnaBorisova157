39a17862f1fd53508a3b0861d485a75271940738
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-05 00:08:24 +0100

    Update task7.py
     even numbers

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task7():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT x
+    Loop x from 1 to 100
+    IF x divided of 2 then
+    OUTPUT x
+    END if
+    END loop
+    """
