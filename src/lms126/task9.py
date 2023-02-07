e9761e5b4e985a08c1ead02fb22b68a0fb82f3f6
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-07 10:41:10 +0100

    withdraws money

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -3,36 +3,26 @@ from to_do import TODO
 
 def task9():
     return """
-input Amount from customer keypad
-    read customer's details from accounts database
-    IF customer has insufficient funds
-        THEN IF customer has zero funds
-            THEN BEGIN
-                display 'insufficient funds' message
-                set Amount to zero
-            END
-            ELSE BEGIN
-                display offer of available funds
-                input customer's response (Y/N)
-                IF response is 'Y'
-                    THEN set reduced Amount
-                    ELSE set Amount to zero
-                ENDIF
-            END
-        ENDIF
-    ENDIF
-    return customer's card
-    IF Amount is not zero
-        THEN BEGIN
-            dispense cash Amount
-            IF WithdrawalType is with_receipt
-                THEN print receipt for Amount
-            ENDIF
-            update accounts database
-        END
-    ENDIF
+ input Amount from customer keypad
+ read customer's details from accounts database
+ IF customer has insufficient funds
+ THEN IF customer has zero funds
+ THEN BEGIN display 'insufficient funds' message
+ set Amount to zero
+END
+ELSE BEGIN display offer of available funds
+INPUT customer's response (Y/N)
+IF response is 'Y'
+THEN set reduced Amount
+ELSE set Amount to zero
+ENDIF
+END
+return customer's card
+IF Amount is not zero
+THEN BEGIN dispense cash Amount
+IF WithdrawalType is with_receipt
+THEN print receipt for Amount
+ENDIF
+update accounts database
 END Withdrawal
-
-
-
 """
