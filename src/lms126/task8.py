f6853ead4f654e62c96de85e78fef8292fd2482c
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-07 10:33:56 +0100

    task 8 numbers 3 or 5

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT x
+    IF x multiplies of 3
+     Print Foo
+    IF x multiplies of 5
+     Print Bar
+    IF x multiplies of 3 and 5
+     Print FooBar
+    IF x not multiplies of 3 or 5
+     Print Qix
+    OUTPUT x
+    """
