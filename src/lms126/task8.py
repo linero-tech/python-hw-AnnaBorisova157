7d0c456a1f19ee172eaa17028fddeb237a031edd
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-08 17:51:32 +0100

    task 8 after lesson

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -8,8 +8,6 @@ def task8():
      Print Foo
     IF x multiplies of 5
      Print Bar
-    IF x multiplies of 3 and 5
-     Print FooBar
     IF x not multiplies of 3 or 5
      Print Qix
     OUTPUT x
