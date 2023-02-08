9d8e1551da63f756559c9944b6d52f7eb102408e
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-08 18:06:31 +0100

    Up task 10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -6,7 +6,9 @@ def task10():
     INPUT password
     INPUT pass_length 
     If pass_length is greater than 6 
-    Print valid password
-otherwise:
-  invalid password
+     Print valid password
+     OUTPUT password
+    If pass_length is less than 6 
+    Print invalid password
+    Start again
     """
