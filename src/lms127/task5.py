ae47cdfbe623da6f95f34b8bc3f08a53280c0d3e
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-22 13:44:19 +0100

    Task 5 mod 102

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -4,8 +4,9 @@ from to_do import TODO
 def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
-
-    TODO("Your solutions goes in here.")
-
+    a,b = b,a
     # Do not erase or change the below statement
     return a, b
+if __name__ == "__main__":
+    result = task5(value_for_a=1, value_for_b=2)
+    print(result)
