ebc5be66c773b1269f56e47de6375207563ff2cd
AnnaBorisova157 <go.borisova@gmail.com>
2023-02-22 14:01:37 +0100

    Task 7 mod 102

--- src/lms127/task7.py
+++ src/lms127/task7.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task7(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return
+sentence = 'I love GBG'
+print(len(sentence))
+
+if __name__ == "__main__":
+        task7(sentence)
