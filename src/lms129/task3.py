859147327deea42045984f6f3fc9a7ae71c97840
AnnaBorisova157 <go.borisova@gmail.com>
2023-03-07 12:15:57 +0100

    Task 3 mod 3

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -2,6 +2,20 @@ from to_do import TODO
 
 
 def task3(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    if number < 0:
+        return 0
+    elif number == 0 or number == 1:
+        return 1
+    else:
+        fact = 1
+        while (number > 1):
+            fact *= number
+            number -= 1
+    return fact
+
+
+
+if __name__ == "__main__":
+    print(task3(number=5))  # 120
+    print(task3(number=7))  # 5040
+    print(task3(number=3))  # 6
