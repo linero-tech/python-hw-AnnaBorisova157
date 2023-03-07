4919adf6c8be3012d2892f008a79257e9be5e9f7
AnnaBorisova157 <go.borisova@gmail.com>
2023-03-07 11:58:01 +0100

    Task 2 mod 3

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,11 +2,17 @@ from to_do import TODO
 
 
 def task2(number):
-    if (number > 1) result = True
-    if (number)
-    return result
+    if number < 2:
+        return False
+    for i in range (2, int ( number ** 0.5 + 1)):
+        if number % i == 0:
+            return False
+    else:
+        return True
+
 
 
 if __name__ == "__main__":
     print(task2(number=5))  # True
-    print(task2(number=2))  # False
+    print(task2(number=1))  # False
+    print(task2(number=3))  # True
